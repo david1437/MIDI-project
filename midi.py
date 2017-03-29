2421b54e4740865e8aa574f7cb0819fab605dce7
David Sherrier <davidsherrier1997@gmail.com>
2017-03-29 00:23:13 -0400

    Added writing data to file

--- midi.py
+++ midi.py
@@ -11,16 +11,16 @@ event_post = pygame.fastevent.post
 pygame.midi.init()
 i = pygame.midi.Input( 2 )
 
-
 pygame.display.set_caption("midi test")
 screen = pygame.display.set_mode((400, 300), RESIZABLE, 32)
 
 print "starting"
-
+f = open('output', 'w')
 going = True
 
-while going:
+time = 0
 
+while going:
         events = event_get()
         for e in events:
                 if e.type in [QUIT]:
@@ -30,13 +30,17 @@ while going:
 
         if i.poll():
                 midi_events = i.read(10)
-                print "full midi_events " + str(midi_events)
+                print "MIDI EVENT " + str(midi_events) + "\n"
+                for index in range(len(midi_events)):
+                        f.write(str(midi_events[index]) + "\n")
                 midi_evs = pygame.midi.midis2events(midi_events, i.device_id)
 
                 for m_e in midi_evs:
                         event_post( m_e )
 
 print "exit button clicked."
+f.close()
+i.close()
 del i
 pygame.midi.quit()
 pygame.quit()
