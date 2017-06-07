825e052fb2ff5cb417daf51918c7bf4bc7efed43
David Sherrier <davidsherrier1997@gmail.com>
2017-06-07 17:16:40 -0400

    Add files via upload

--- midi.py
+++ midi.py
@@ -45,6 +45,7 @@ else:
 
 screen.fill((0,0,0))        
 f = open('output.txt', 'w')
+f.write('{}    {}    {}    {}    {}    {}\n'.format("Timestamp", "Event Code", "Note Name", "Note Octave", "Velocity", "Duration"))
 
 note_list = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 midi_data_list = []
@@ -62,7 +63,7 @@ while going:
                         going = False
 
         if i.poll():
-                midi_events = i.read(10)
+                midi_events = i.read(100)
                 for index in range(len(midi_events)):
                         midi_data_list.append(midi_events[index])
                 midi_evs = pygame.midi.midis2events(midi_events, i.device_id)
@@ -72,30 +73,33 @@ while going:
         pygame.display.update()
         
 for index, midi_data in enumerate(midi_data_list):
-        f.write("Time: " + str(midi_data[1]) + "ms ")
+        timestamp = str(midi_data[1])+"ms"
         if midi_data[0][0] < 144:
-                f.write("Event: Off ")
+                note_press = "Off"
                 event = True
         else:
-                f.write("Event: On ")
+                note_press = "On"
                 event = False
         if midi_data[0][1] <= 11:
-                f.write("Note Name: " + note_list[midi_data[0][1]] + " Note Octave: 0 ")
+                note = note_list[midi_data[0][1]]
+                octave = "0"
         else:
-                f.write("Note Name: " + note_list[midi_data[0][1] % 12] + " Note Octave: " + str(midi_data[0][1]/12) + " ")
+                note = note_list[midi_data[0][1] % 12]
+                octave = str(midi_data[0][1]/12)
         if event is True:
-                f.write("Velocity: " + str(midi_data[0][2]) + " Duration: ")
-                timestamp = midi_data[1]
-                note = midi_data[0][1]
+                duration = "-"
+                velocity = str(midi_data[0][2])
                 search_list = midi_data_list[0:index]
                 for search_data in reversed(search_list):
-                        if search_data[1] < timestamp and search_data[0][0] >= 144 and search_data[0][1] == note:
-                                duration = timestamp - search_data[1]
+                        if search_data[1] < midi_data[1] and search_data[0][0] >= 144 and search_data[0][1] == midi_data[0][1]:
+                                duration = str(midi_data[1] - search_data[1])
                                 break
-                f.write(str(duration) + "ms\n")
         else:
-                f.write("Velocity: " + str(midi_data[0][2]) + "\n")           
-              
+                velocity = str(midi_data[0][2])
+                duration = "-"        
+        output = '{:10}   {:10}    {:10}   {:10}     {:10}    {:10}\n'.format(timestamp, note_press, note, octave, velocity, duration)
+        f.write(output)
+
 f.close()
 i.close()
 del i
