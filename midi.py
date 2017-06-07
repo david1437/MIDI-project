6c1d25376f58ec1c19e9b5db478797d50e85dd2e
David Sherrier <davidsherrier1997@gmail.com>
2017-06-07 03:36:46 -0400

    Added a lot of features
    check the readme file for more information, just needs to be tested

--- midi.py
+++ midi.py
@@ -1,26 +1,59 @@
-import pygame
+import pygame, sys
 import pygame.midi
+import inputbox
 from pygame.locals import *
 
 pygame.init()
 
+pygame.font.init()
 pygame.fastevent.init()
 event_get = pygame.fastevent.get
 event_post = pygame.fastevent.post
 
 pygame.midi.init()
-i = pygame.midi.Input( 2 )
 
-pygame.display.set_caption("midi test")
+pygame.display.set_caption("Midi Data Analyzer")
 screen = pygame.display.set_mode((400, 300), RESIZABLE, 32)
 
-print "starting"
-f = open('output', 'w')
+myfont = pygame.font.SysFont('monospace', 15)
+surfacefont = myfont.render('Port Opened Sucessfully!',True,(255,255,255), (0,0,0))
+surfacer = surfacefont.get_rect()
+surfacer.center = (200,150)
+
+user_input = str(inputbox.ask(screen, 'Enter Port Number'))
+
+if(user_input != ""):
+        try:
+                i = pygame.midi.Input( int(user_input) )
+        except Exception:
+                screen.fill((0,0,0))        
+                surfacefont = myfont.render('Invalid Port Number!',True,(255,255,255), (0,0,0))
+                screen.blit(surfacefont, surfacer)
+                pygame.display.update()
+                pygame.time.wait(5000)
+                exit()                
+else:
+        try:
+                i = pygame.midi.Input( pygame.midi.get_default_input_id() )
+        except Exception:
+                screen.fill((0,0,0))        
+                surfacefont = myfont.render('No port detected try reconnecting your device!',True,(255,255,255), (0,0,0))
+                screen.blit(surfacefont, surfacer)
+                pygame.display.update()
+                pygame.time.wait(5000)
+                exit()
+
+screen.fill((0,0,0))        
+f = open('output.txt', 'w')
+
+note_list = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+midi_data_list = []
 going = True
 
 time = 0
 
 while going:
+        screen.blit(surfacefont, surfacer)
         events = event_get()
         for e in events:
                 if e.type in [QUIT]:
@@ -30,15 +63,39 @@ while going:
 
         if i.poll():
                 midi_events = i.read(10)
-                print "MIDI EVENT " + str(midi_events) + "\n"
                 for index in range(len(midi_events)):
-                        f.write(str(midi_events[index]) + "\n")
+                        midi_data_list.append(midi_events[index])
                 midi_evs = pygame.midi.midis2events(midi_events, i.device_id)
 
                 for m_e in midi_evs:
                         event_post( m_e )
-
-print "exit button clicked."
+        pygame.display.update()
+        
+for index, midi_data in enumerate(midi_data_list):
+        f.write("Time: " + str(midi_data[1]) + "ms ")
+        if midi_data[0][0] < 144:
+                f.write("Event: Off ")
+                event = True
+        else:
+                f.write("Event: On ")
+                event = False
+        if midi_data[0][1] <= 11:
+                f.write("Note Name: " + note_list[midi_data[0][1]] + " Note Octave: 0 ")
+        else:
+                f.write("Note Name: " + note_list[midi_data[0][1] % 12] + " Note Octave: " + str(midi_data[0][1]/12) + " ")
+        if event is True:
+                f.write("Velocity: " + str(midi_data[0][2]) + " Duration: ")
+                timestamp = midi_data[1]
+                note = midi_data[0][1]
+                search_list = midi_data_list[0:index]
+                for search_data in reversed(search_list):
+                        if search_data[1] < timestamp and search_data[0][0] >= 144 and search_data[0][1] == note:
+                                duration = timestamp - search_data[1]
+                                break
+                f.write(str(duration) + "ms\n")
+        else:
+                f.write("Velocity: " + str(midi_data[0][2]) + "\n")           
+              
 f.close()
 i.close()
 del i
