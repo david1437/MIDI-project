c17775678a898a673baac71f39d421afc9f31fe8
David Sherrier <davidsherrier1997@gmail.com>
2017-06-15 00:21:06 -0400

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-import cx_Freeze
-
-executables = [cx_Freeze.Executable("midi.py")]
-
-cx_Freeze.setup(
-        name="Midi Analyzer",
-        options={"build_exe":{"packages":["pygame"], "include_files":["freesansbold.ttf"]}},
-        executables = executables
-        )
